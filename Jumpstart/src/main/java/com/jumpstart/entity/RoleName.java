545562c393795ab407f6bcbdc8e32a8544782aa1
Christian Roy Sevilla <110905428+Chriszy00@users.noreply.github.com>
2023-10-09 12:42:59 +0800

    Delete Jumpstart directory

--- Jumpstart/src/main/java/com/jumpstart/entity/RoleName.java
+++
@@ -1,9 +0,0 @@
-package com.jumpstart.entity;
-
-public enum RoleName {
-
-	ADMINISTRATOR, // Correct spelling
-	USER,
-	MEMBER,
-
-}
--- Jumpstart/src/main/java/com/jumpstart/repository/PaymentSummaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jumpstart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jumpstart.entity.PaymentSummary;
-
-public interface PaymentSummaryRepository extends JpaRepository<PaymentSummary, Long> {
-    // Define any custom queries or methods if needed
-}
